822989ea68eb6c5eabec459f4b4e27c37d8552fe
Franco Luna <Lunafranco983@gmail.com>
2025-09-08 20:53:03 -0300

    funcion para encontrar base de datos y app de ruta principal(necesaria) para que corra otras partes del programa

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template,request,redirect,url_for,session,flash
 import sqlite3
+import os
 
 
 app = Flask(__name__)
 app.secret_key = "admin"
 
 def get_db_connection():
-    conn = sqlite3.connect('basededatosflask.db')
+    # Esto busca en la carpeta del proyecto
+    db_path = os.path.join(os.path.dirname(__file__), 'basededatosflask.db')
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
+
+@app.route("/")
+def home():
+    return redirect(url_for("login"))
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
